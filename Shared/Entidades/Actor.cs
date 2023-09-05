00bfa64df2ea157254b93395f5fd3de917a7492f
alejoatria <alejoatria@gmail.com>
2023-09-05 15:48:45 -0500

    clase 64 done. Funcionando carga de imagenes. Resuelto problema por error de InputDate

---
+++ Shared/Entidades/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; } = null!;
+        public string? Biografia { get; set; }
+        public string? Foto { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+
+    }
+}
