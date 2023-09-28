69ea0b370070c8d7673026236205a9bb5bc2bc11
alejoatria <alejoatria@gmail.com>
2023-09-28 13:15:43 -0500

    added MD and genre selector

--- Shared/Entidades/Pelicula.cs
+++ Shared/Entidades/Pelicula.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,16 @@ namespace BlazorPeliculas.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; } = null!;
-        public DateTime FechaLanzamiento { get; set; }
-        public string Poster { get; set; } = null!;
-        public string TituloCortado 
+        public string? Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string? Trailer { get; set; }
+        public DateTime? Lanzamiento { get; set; }
+        public string? Poster { get; set; } = null!;
+        public List<GeneroPelicula> GenerosPeliculas { get; set; } = new List<GeneroPelicula>();
+        public string? TituloCortado 
         {
             get
             {
